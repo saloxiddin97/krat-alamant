dd71c5cd83bbcfa28dffe6d6d3b12dc87c375529
saloxiddin97 <salohiddinshamsidinov0101@gmail.com>
2024-07-15 14:04:35 +0500

    krat alamnt

---
+++ scrip.js
@@ -0,0 +1,8 @@
+let h1=document.querySelector("h1")
+let h2=document.getElementById("olma")
+let a=prompt("ism kriting")
+h2.textContent = a
+
+let rang=prompt("rangb kriting")
+let body=document.querySelector("body")
+   body.style.background=rang
